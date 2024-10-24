fc3e470752bbf6861bdad597112d73f54f1c5aae
nguyenquoctinh <nguyentinh140321@gmail.com>
2024-10-24 17:13:24 +0700

    check Tinh

--- assets/js/Sub-Main.js
+++ assets/js/Sub-Main.js
@@ -1,17 +1,28 @@
-  $(document).ready(function() {
-    $('input[type="checkbox"]').change(function () {
-      const label = $(this).next('label').text();
-      const id = $(this).attr('id');
-      if (this.checked) {
-        $('#tagContainer').append(`<div class="tag-item-${id}"><span class="tag">${label}</span> <span class="close" data-id="${id}">X</span></div>`);
-      } else {
-        $(`.tag-item-${id}`).remove();
+$(document).ready(function () {
+  $('input[type="checkbox"]').change(function () {
+    const label = $(this).next('label').text();
+    const id = $(this).attr('id');
+    // Change background color of the header based on checked state
+    if (this.checked) {
+      $('#tagContainer').append(`<div class="tag-item" id="tag-${id}"><span class="tag">${label}</span> <span class="close" data-id="${id}">X</span></div>`);
+      $('.header--filter').addClass('checked'); // Change background when checked
+    } else {
+      $(`#tag-${id}`).remove();
+      if ($('input[type="checkbox"]:checked').length === 0) {
+        $('.header--filter').removeClass('checked'); // Remove background if no checkboxes are checked
       }
-    });
-
-  $(document).on('click', '.close', function() {
-                const id = $(this).data('id');
-  $(`#${id}`).prop('checked', false);
-  $(this).parent().remove();
-            });
-        });
+    }
+  });
+  $('input[type="radio"]').change(function () {
+    const label = $(this).next('label').text();
+    const id = $(this).attr('id');
+    if (this.checked) {
+      $('#tagContainer').children('.tag-radio').html(`<div class="tag-item tag-radio" id="tag--${id} tag-radio"><span class="tag">${label}</span> <span class="close" data-id="${id}">X</span></div>`)
+    }
+  });
+  $(document).on('click', '.close', function () {
+    const id = $(this).data('id');
+    $(`#${id}`).prop('checked', false).change(); // Trigger change to update state
+    $(this).parent().remove();
+  });
+});
